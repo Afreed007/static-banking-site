88d6403204e706ae8a89f7cd6e0a10229040a693
Afreed007 <afreed0076@gmail.com>
2024-06-27 17:09:22 +0530

    added billing business button card clients cta feedback and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./style";
 
-import { Navbar, Hero,Business,Stats } from "./components";
+import { Navbar, Hero,Business,Stats,Billing, CardDeal, Testimonials ,Clients , CTA ,Footer } from "./components";
 const App = () => {
   return (
     <div className="bg-primary w-full overflow-hidden">
@@ -16,10 +16,16 @@ const App = () => {
         </div>
       </div>
 
-      <div className={"bg-primary ${styles.paddingX} ${styles.flexCenter}"}>
+      <div className={"bg-primary ${styles.paddingX} ${styles.flexCenter} ml-10"}>
         <div className="${styles.boxWidth}">
           <Stats />
           <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
         </div>
         
       </div>
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,13 @@ import Navbar from "./Navbar";
 import Hero from "./Hero";
 import GetStarted from "./GetStarted";
 import Stats from "./Stats";
-import Business from "./Business";
-export { Navbar, Hero, GetStarted, Stats, Business };
+import Business from "./Business";import Button from "./Button";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Testimonials from "./Testimonials";
+import FeedbackCard from "./FeedbackCard";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Footer from "./Footer";
+export { Navbar, Hero, GetStarted, Stats, Business, Button, Billing, CardDeal, Testimonials, FeedbackCard, Clients, Footer, CTA };
+
