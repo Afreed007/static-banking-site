333a8dbd76ee9d3749f493a2d2e419062a2ea5b0
Afreed007 <afreed0076@gmail.com>
2024-07-23 13:33:51 +0530

    location

--- src/test.js
+++ src/test.js
@@ -1,8 +1,12 @@
 if("geolocation" in navigator){
     navigator.geolocation.getCurrentPosition(function(position){
         let lat = position.coords.latitude;
-        let long = position.coords.longitude;});
+        let long = position.coords.longitude;
+        
+    });
+        
     }
 else{
-
+    console.log("Geolocation is not supported by this browser.");
 }
+
